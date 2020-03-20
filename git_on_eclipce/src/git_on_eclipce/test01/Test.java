5de6a0fc3c2ba9247bd7090b314ef07050525202
shyani20 <shyani20@gmail.com>
2020-03-21 02:04:32 +0900

    Add New Project [git_on_eclipce]

---
+++ git_on_eclipce/src/git_on_eclipce/test01/Test.java
@@ -0,0 +1,8 @@
+package git_on_eclipce.test01;
+
+public class Test {
+	public static void main (String [] args) {
+		System.out.println("**이클립스에서 git 연동하기**");
+		System.out.println("-Egit 이용-");
+	}
+}
